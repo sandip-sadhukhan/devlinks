fb5e24ed88c093057eda786e69c2cf702294cdfe
Sandip Sadhukhan <sandippakhanna@gmail.com>
2024-05-15 08:49:29 +0530

    login form

--- link/urls.py
+++ link/urls.py
@@ -3,5 +3,6 @@ from link import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login/", views.login, name="login")
+    path("login/", views.login, name="login"),
+    path("dashboard/", views.dashboard, name="dashboard"),
 ]
